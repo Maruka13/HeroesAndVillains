1ca09b438325e736178e680331b2e8abf2f00537
Maruka <emanuelle_carollina@hotmail.com>
2025-04-07 02:32:15 -0300

    sorteio de nomes

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ let forcaPersonagem = 0;
 let forcaViloes = 0;
 
 function iniciarJogo() {
-    for (let i = 0; i < 3; i++) {
+    for(let i = 0; i < 3; i++) {
         let escolhaPersonagem = prompt("Digite o nome do seu Herói " + (i + 1) + ":");
         personagem[i] = escolhaPersonagem;
     }
+    console.log("Agora,o Array personagem contém: " + personagem)
+
+    for(let i = 0; i < 3; i++) {
+        indiceAleatorio = Math.floor(Math.random() * 10)
+    }
+
+
 
     // Exibe os nomes no HTML
     document.getElementById("informacoesJogador").innerText = "Seus heróis: " + personagem.join(", ");
